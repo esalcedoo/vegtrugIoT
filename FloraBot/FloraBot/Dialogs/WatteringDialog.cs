9f73afc199db57e882e0ae7915614ae9d9d783be
esalcedoo <elena@esalcedoo.com>
2021-04-22 10:28:28 +0200

    wattering intent  handler & dialog

---
+++ FloraBot/FloraBot/Dialogs/WatteringDialog.cs
@@ -0,0 +1,42 @@
+﻿using FloraBot.Services.Flora;
+using FloraBot.Services.IoTCentral;
+using FloraModels;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FloraBot.Dialogs
+{
+    public class WatteringDialog : Dialog
+    {
+        private readonly FloraService _floraService;
+        private readonly IoTCentralService _ioTCentralService;
+
+        public WatteringDialog(FloraService floraService, IoTCentralService ioTCentralService) : base(nameof(WatteringDialog))
+        {
+            _floraService = floraService;
+            _ioTCentralService = ioTCentralService;
+        }
+        public override async Task<DialogTurnResult> BeginDialogAsync(DialogContext dc, object options = null, CancellationToken cancellationToken = default)
+        {
+            int plantId = GetPlanId(dc);
+            PlantModel plantModel = await _floraService.GetPlantInfo(plantId);
+            await dc.Context.SendActivityAsync(MessageFactory.Text(plantModel.ToString()), cancellationToken);
+            return await dc.EndDialogAsync(cancellationToken: cancellationToken);
+        }
+
+        private int GetPlanId(DialogContext dc)
+        {
+            //RecognizerResult luisResult = dc.TurnState
+            //                .Get<RecognizerResult>("LuisRecognizerResult");
+            ////TODO
+            //return luisResult.Entities.ContainsKey("now");
+
+            return 1;
+        }
+    }
+}
---
+++ FloraBot/FloraBot/IntentHandlers/LUISWatteringIntentHandler.cs
@@ -0,0 +1,40 @@
+﻿using FloraBot.Dialogs;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Schema;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FloraBot.IntentHandlers
+{
+    public class LUISWatteringIntentHandler : LUISIntentHandler
+    {
+        private readonly ConversationState _conversationState;
+        private readonly WatteringDialog _watteringDialog;
+
+        public LUISWatteringIntentHandler(ConversationState conversationState, WatteringDialog watteringDialog)
+        {
+            _conversationState = conversationState;
+            _watteringDialog = watteringDialog;
+        }
+
+        public override async Task<DialogTurnResult> Handle(DialogContext dialogContext, CancellationToken cancellationToken)
+        {
+            return await dialogContext.BeginDialogAsync(_watteringDialog.Id, cancellationToken: cancellationToken);
+        }
+
+        public override async Task Handle(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            await _watteringDialog.RunAsync(turnContext,
+                            _conversationState.CreateProperty<DialogState>(nameof(DialogState)), cancellationToken);
+        }
+
+        public override bool IsValid(ITurnContext turnContext)
+        {
+            return GetTopIntentKey(turnContext) == "Wattering";
+        }
+    }
+}
